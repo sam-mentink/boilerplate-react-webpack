af19c85aed4d5c99cb5d266d455e187a20d7d980
EDA <eda@mac-mini-1.local>
2016-09-27 11:58:26 +1300

    get route fetching

--- server/server.js
+++ server/server.js
@@ -2,11 +2,26 @@ var path = require('path')
 var express = require('express')
 var bodyParser = require('body-parser')
 
+var development = require('../knexfile').development
+var knex = require('knex')(development)
+
 var PORT = process.env.PORT || 3000
 var app = express()
 
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '/../client')))
+
+app.get('/todo', function(req, res) {
+  knex('todo')
+    .select()
+    .then(function (todo) {
+      var vm = {
+        todo: todo
+      }
+      res.send(vm)
+    })
+    .catch(console.error)
+})
 
 app.listen(PORT, function () {
   console.log('Listening on port', PORT)
