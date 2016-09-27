4404ccb93d126430f86fef12bbec9fb0ccf8b1cb
EDA <eda@mac-mini-1.local>
2016-09-27 16:46:06 +1300

    wip

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,10 +3,22 @@ import Header from './Header'
 import Addtodo from './Addtodo'
 import Todolist from './Todolist'
 
+
+let data = []
+request
+  .get('http://localhost:3000/todo')
+  .end((err, res) => {
+    console.log(res)
+    if(!err) {
+      data = res.body
+    }
+  })
+
 export default React.createClass({
   render() {
     return (
       <div>
+        <p>Todo Here: </p>
       <Header text="🚬 This to-do list will make you wanna... 🚬" />
       <Addtodo />
       <Todolist text="Gone to get coffee"/>
--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import request from 'superagent'
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
